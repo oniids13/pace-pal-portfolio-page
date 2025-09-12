2c37f87884a9c1eb189b5af7c60a84c422cb8a72
oniids <jdino.abaya@gmail.com>
2025-09-12 13:43:43 +0800

    add product structure section

--- src/components/body_content/ProductStructure.jsx
+++ src/components/body_content/ProductStructure.jsx
@@ -4,6 +4,22 @@ const ProductStructure = () => {
       <h1 className="text-sky-600 text-3xl font-bold mb-12 text-center">
         PRODUCT STRUCTURE
       </h1>
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <p className="text-gray-600 mb-8 text-base max-w-4xl mx-auto text-justify">
+          Based on the opportunity areas I summarized, I defined PacePal's
+          product structure. Users can view run events that are near in their
+          area on the home screen and a CTA button in scheduling a run. There
+          are also buttons for search, run history, runner's profile, and
+          notifications.
+        </p>
+        <div className="flex justify-center">
+          <img
+            className="w-150 h-auto object-contain rounded-lg shadow-md"
+            src="/src/images/IA.png"
+            alt="Product Structure"
+          />
+        </div>
+      </div>
     </div>
   );
 };
