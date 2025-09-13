7736c4deff9bf190bbc540084519e25531099f9e
oniids <jdino.abaya@gmail.com>
2025-09-13 13:36:34 +0800

    make the header sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="">
+    <header className="sticky top-0 z-50 bg-white">
       <div className="navbar bg-base-100 shadow-md flex justify-center items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Logo on the left */}
         <div className="flex-1">
